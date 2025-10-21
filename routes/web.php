798e7004e488729cf0bbe0da3ade5ebca47f63e2
berlindiachica <berlindi24ti@mahasiswa.pcr.ac.id>
2025-10-21 10:23:41 +0700

    Pertemuan 7-Volt Template 2

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\HomeStandardController;
 use App\Http\Controllers\HomeAdvanceController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SignupController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
 use Illuminate\Support\Facades\Route;
 
 // Redirect root ke signup
@@ -21,3 +23,10 @@ Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login')
 // Home Routes untuk kedua tipe user
 Route::get('/home-standard', [HomeStandardController::class, 'index'])->name('home.standard');
 Route::get('/home-advance', [HomeAdvanceController::class, 'index'])->name('home.advance');
+
+//Pertemuan7
+Route::get('dashboard',[DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('pelanggan',[PelangganController::class, 'index'])->name('pelanggan.list');
+Route::get('pelanggan/create',[PelangganController::class, 'create'])->name('pelanggan.create');
+Route::post('pelanggan/store',[PelangganController::class, 'store'])->name('pelanggan.store');
